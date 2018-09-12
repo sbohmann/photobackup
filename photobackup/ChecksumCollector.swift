234562e28f42f3260df23bee57dd57daf2691d5d
sbohmann <mail@sebastian-bohmann.com>
2018-09-12 16:58:10 +0200

    Fixed 

--- photobackup/ChecksumCollector.swift
+++ photobackup/ChecksumCollector.swift
@@ -3,15 +3,15 @@ import Foundation
 
 class ChecksumCollector {
     var digest = [UInt8](repeating: 0, count: Int(CC_SHA512_DIGEST_LENGTH))
-    var state: UnsafeMutablePointer<CC_SHA512_CTX>?
+    var state = CC_SHA512_CTX()
     
     init() {
-        CC_SHA512_Init(state)
+        CC_SHA512_Init(&state)
     }
     
     func handleData(data: Data) {
         NSLog("data size: %d", data.count)
-        _ = data.withUnsafeBytes { pointer in CC_SHA512_Update(state, pointer, CC_LONG(data.count)) }
+        _ = data.withUnsafeBytes { pointer in CC_SHA512_Update(&state, pointer, CC_LONG(data.count)) }
     }
     
     func handleCompletion(error: Error?) {
@@ -19,7 +19,8 @@ class ChecksumCollector {
             NSLog("Error: $@", error.localizedDescription)
         } else {
             NSLog("success :D")
-            CC_SHA512_Final(&digest, state)
+            CC_SHA512_Final(&digest, &state)
+            NSLog("%@", digest.map({ String(format: "%02hhx", $0) }).joined(separator: ""))
         }
     }
 }
